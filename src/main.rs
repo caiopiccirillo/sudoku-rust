70dec3e9a9d7cd488fe12c70a0dfaa0c88b6e5c5
Caio Piccirillo <caio.piccirillo@tecsidel.com>
2020-11-19 20:16:32 -0300

    Adding arguments at main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::env;
+use image::GenericImageView;
+
 mod reader;
 mod solver;
 mod writer;
+mod sudoku;
 
 fn main() {
-    println!("Begin!");
-    let mut my_reader: reader::Reader;
-    my_reader.read_image("/home/caio/Documentos/sudoku-rust/assets/sample.png".to_string());
-        
+    // Read arguments
+    let args: Vec<String> = env::args().collect();
+    // Get image file path
+    let path = &args[1];
+    // Create an instance of reader
+    let my_reader= reader::Reader::new(path.into());
+    let sudoku_board = sudoku::Sudoku::new();
+    println!("{:?}",sudoku_board);
+    my_reader.process_image(sudoku_board);
+    println!("{:?}",my_reader.img.dimensions());
 }
