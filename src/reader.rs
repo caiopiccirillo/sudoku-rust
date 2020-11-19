20391f21ac95cf1eab40242f50d9de8f4c76dfa4
Caio Piccirillo <caio.piccirillo@tecsidel.com>
2020-11-19 20:16:08 -0300

    Changing reader

--- src/reader.rs
+++ src/reader.rs
@@ -1,5 +1,7 @@
-use image;
+/// This module will read the image file and extract the information from its file.
+use super::sudoku;
 pub struct Reader {
+    // TODO: img to Some(Box<DynamicImage>)
     pub img: image::DynamicImage,
 }
 
@@ -9,7 +11,18 @@ impl Reader {
             img: image::open(path).unwrap(),
         }
     }
-    pub fn read_image(&mut self,path: String){
+    pub fn read_image(&mut self, path: String) {
         self.img = image::open(path).unwrap()
+    }
+    pub fn process_image(&self, board: sudoku::Sudoku) {
+        todo!();
+        self.img.save("assets/test.png").unwrap();
+    }
+}
+
+impl std::fmt::Debug for Reader{
+
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+       todo!()
     }
 }
